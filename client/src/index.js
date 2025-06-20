854735604d63f7b7b095dd66ae745b72bc73beb2
Mohamed <mohamed.taher@esprit.tn>
2025-06-20 10:46:31 +0100

    translate

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './i18n'; // Import de la configuration i18n
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 const rootElement = document.getElementById('root');
